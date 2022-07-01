153ac4bbfe202115ed99f0c00288d36f4181410e
Felipe Schneider <felipeschneider2811@gmail.com>
2022-07-01 12:04:09 -0300

    Adicionando CRUD da tabela tema

--- Programa Springboot/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagens.java
+++ Programa Springboot/blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagens.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagens")
@@ -25,11 +28,15 @@ public class Postagens {
 	private String titulo;
 	
 	@NotNull
-	@Size(min = 5, max = 100, message = "O campo necessita ter pelo menos 5 letras")
+	@Size(min = 5, max = 5000, message = "O campo necessita ter pelo menos 5 letras")
 	private String texto;
 	
 	@UpdateTimestamp
 	private LocalDateTime date;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -61,6 +68,14 @@ public class Postagens {
 
 	public void setDate(LocalDateTime date) {
 		this.date = date;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
